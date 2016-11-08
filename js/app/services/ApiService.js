ec86ad27bbd14765260ffa291b826b3f6c3b8b68
Marcos Ginel <marcos.ginel@gmail.com>
2016-11-08 01:47:19 +0100

    * Refactor del ApiService v2      -> Falta: Poder calcular una "factura" completa (DateTime + C3)      -> Falta: Poder modificar un consumo de un usuario

--- js/app/services/ApiService.js
+++ js/app/services/ApiService.js
@@ -14,19 +14,7 @@ angular.module("wellnessApp").service("ApiService", ["$http", "configuracion", '
     };
 
     this.logout = function() {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiLogout,
-            method: 'GET',
-            headers : cabecera
-        };*/
-        peticion = creaPeticion('GET',configuracion.rutaApiLogout,undefined, undefined);
-        $http(peticion).then(
+        $http(creaPeticion('GET',configuracion.rutaApiLogout,undefined, undefined)).then(
             function( resultado ) {
                 console.log("Deslogeado");
                 $window.location.href = '';
@@ -40,35 +28,11 @@ angular.module("wellnessApp").service("ApiService", ["$http", "configuracion", '
     };
 
     this.getUsers = function() {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetUsers,
-            method: 'GET',
-            headers : cabecera
-        };*/
-        peticion = creaPeticion('GET', configuracion.rutaApiGetUsers, undefined, undefined);
-        return $http(peticion);
+        return $http(creaPeticion('GET', configuracion.rutaApiGetUsers, undefined, undefined));
     };
 
     this.getUserFromApi = function(id) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetUsers+id+"/",
-            method: 'GET',
-            headers : cabecera
-        };*/
-        peticion = creaPeticion('GET', configuracion.rutaApiGetUsers, undefined, id);
-        return $http(peticion);
+        return $http(creaPeticion('GET', configuracion.rutaApiGetUsers, undefined, id));
     };
 
     this.saveToken = function(token, username) {
@@ -87,67 +51,18 @@ angular.module("wellnessApp").service("ApiService", ["$http", "configuracion", '
     };
 
     this.deleteUser = function(id) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiDeleteUser+id+"/",
-            method: 'DELETE',
-            headers : cabecera
-        };*/
-        peticion = creaPeticion('DELETE', configuracion.rutaApiDeleteUser, undefined, id);
-        return $http(peticion);
+        return $http(creaPeticion('DELETE', configuracion.rutaApiDeleteUser, undefined, id));
     };
 
     this.modificaUsuario = function(usuario, id) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetUsers+id+"/",
-            method: 'PUT',
-            headers : cabecera,
-            data : usuario
-        };*/
-        peticion = creaPeticion('PUT', configuracion.rutaApiGetUsers, usuario, id);
-        return $http(peticion);
+        return $http(creaPeticion('PUT', configuracion.rutaApiGetUsers, usuario, id));
     };
 
     this.creaUsuario = function(usuario) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetUsers,
-            method: 'POST',
-            headers : cabecera,
-            data : usuario
-        };*/
-        peticion = creaPeticion('POST', configuracion.rutaApiGetUsers, usuario, undefined);
-        return $http(peticion);
+        return $http(creaPeticion('POST', configuracion.rutaApiGetUsers, usuario, undefined));
     }
 
     this.getConsumos = function(id) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetConsumos+id,
-            method: 'GET',
-            headers : cabecera,
-        };*/
         peticion = creaPeticion('GET',configuracion.rutaApiGetConsumos, undefined, id);
         // Necesitamos quitar el ultimo '/' de la petición de búsqueda
         peticion.url = peticion.url.slice(0,-1);
@@ -155,82 +70,19 @@ angular.module("wellnessApp").service("ApiService", ["$http", "configuracion", '
     }
 
     this.getPrecio = function(id, iterador) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetPrecio+id+"/",
-            method: 'GET',
-            headers : cabecera,
-        };*/
-        peticion = creaPeticion('GET', configuracion.rutaApiGetPrecio, undefined, id);
-        return $http(peticion);
+        return $http(creaPeticion('GET', configuracion.rutaApiGetPrecio, undefined, id));
     };
 
     this.getPrecios = function() {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetPrecio,
-            method: 'GET',
-            headers : cabecera,
-        };*/
-        peticion = creaPeticion('GET',configuracion.rutaApiGetPrecio,undefined, undefined );
-        return $http(peticion);
+        return $http(creaPeticion('GET',configuracion.rutaApiGetPrecio,undefined, undefined ));
     };
 
     this.editarPrecio = function(json) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        datos = {
-            fecha:json.fecha,
-            precio:json.precio
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetPrecio+json.id+"/",
-            method: 'PUT',
-            headers : cabecera,
-            data: datos
-        };*/
-        datos = {
-            fecha:json.fecha,
-            precio:json.precio
-        };
-
-        peticion = creaPeticion('PUT', configuracion.rutaApiGetPrecio, datos, json.id);
-
-        return $http(peticion);
+        return $http(creaPeticion('PUT', configuracion.rutaApiGetPrecio, datos, json.id));
     }
 
     this.crearPrecio = function(precio) {
-        /*token = "Token " + this.getToken();
-
-        cabecera = {
-            'Authorization' : token
-        };
-
-        peticion = {
-            url:configuracion.protocol+"://"+configuracion.host+"/"+configuracion.rutaApiGetPrecio,
-            method: 'POST',
-            headers : cabecera,
-            data : precio
-        };*/
-
-        peticion = creaPeticion('POST', configuracion.rutaApiGetPrecio, precio, undefined);
-
-        return $http(peticion);
+        return $http(creaPeticion('POST', configuracion.rutaApiGetPrecio, precio, undefined));
     }
 
     function creaPeticion(metodo, ruta, datos, argumentoUrl) {
